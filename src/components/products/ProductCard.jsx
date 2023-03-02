02f5b55f20586690575f09baf92a89b428e03f11
zumi0102 <tanakafuka0102@gmail.com>
2023-03-02 22:57:23 +0900

    [chapter17] 商品編集の機能を実装

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -7,6 +7,10 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import NoImage from '../../assets/img/src/no_image.png';
 import { useDispatch, useSelector } from 'react-redux';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -64,6 +68,16 @@ const ProductCard = (props) => {
   const images = props.images.length > 0 ? props.images : [NoImage];
   const price = props.price.toLocaleString();
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Card className={classes.root}>
       <CardMedia
@@ -81,6 +95,19 @@ const ProductCard = (props) => {
             ¥{price}
           </Typography>
         </div>
+        <IconButton onClick={handleClick}>
+          <MoreVertIcon />
+        </IconButton>
+        <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+          <MenuItem
+            onClick={() => {
+              dispatch(push('/product/edit/' + props.id));
+              handleClose();
+            }}
+          >
+            編集する
+          </MenuItem>
+        </Menu>
       </CardContent>
     </Card>
   );
