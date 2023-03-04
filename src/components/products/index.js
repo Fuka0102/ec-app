197207c87a3bffe9976fe07e891d5b5aa96d3a3e
zumi0102 <tanakafuka0102@gmail.com>
2023-03-04 17:01:56 +0900

    [chapter21] CartListItemコンポーネントを実装

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -3,4 +3,5 @@ export { default as ImagePreview } from './ImagePreview';
 export { default as SetSizeArea } from './SetSizeArea';
 export { default as ProductCard } from './ProductCard';
 export { default as ImageSwiper } from './ImageSwiper';
+export { default as CartListItem } from './CartListItem';
 export { default as SizeTable } from './SizeTable';
