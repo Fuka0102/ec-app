d7ee6dd9afa51b33f0d7c7480ec24184efa63534
zumi0102 <tanakafuka0102@gmail.com>
2023-03-04 13:08:09 +0900

    [chapter18] ProductDetailコンポーネント実装

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router';
 import { SignIn, Home, SignUp, Reset, ProductEdit } from './templates';
 import Auth from './Auth';
 import ProductList from './templates/ProductList';
+import ProductDetail from './templates/ProductDetail';
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
       <Route exact path='/signin/reset' component={Reset} />
       <Auth>
         <Route exact path='(/)?' component={ProductList} />
+        <Route exact path='/product/:id' component={ProductDetail} />
         <Route path='/product/edit(/:id)?' component={ProductEdit} />
       </Auth>
     </Switch>
