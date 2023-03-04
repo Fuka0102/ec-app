7f97ca3003f0665e2d65648c86fa904a681e41d1
zumi0102 <tanakafuka0102@gmail.com>
2023-03-04 14:59:45 +0900

    [chapter19] Headerコンポーネントを作成

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import { useSelector } from 'react-redux';
+import ToolBar from '@material-ui/core/ToolBar';
+import { getSignedIn } from '../../reducks/users/selectors';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuBar: {
+    backgroundColor: '#fff',
+    color: '#444',
+  },
+  toolbar: {
+    margin: '0 auto',
+    maxWidth: 1024,
+    width: '100%',
+  },
+  iconButtons: {
+    margin: '0 0 0 auto',
+  },
+}));
+
+const Header = (props) => {
+  const classes = useStyles();
+  const selector = useSelector((state) => state);
+  const isSignedIn = getSignedIn(selector);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='fixed' className={classes.menuBar}>
+        <ToolBar className={classes.toolBar}></ToolBar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
