e3ba45d8a14a7bb2e6038e15af2bedae215ac286
zumi0102 <tanakafuka0102@gmail.com>
2023-03-04 14:26:36 +0900

    [chapter18] swiper実装

--- src/templates/ProductDetail.jsx
+++ src/templates/ProductDetail.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { db } from '../firebase';
 import { makeStyles } from '@material-ui/styles';
 import HTMLReactParser from 'html-react-parser';
+import { ImageSwiper } from '../components/products';
 
 const useStyles = makeStyles((theme) => ({
   sliderBox: {
@@ -64,7 +65,9 @@ const ProductDetail = () => {
     <section className='c-section-wrapin'>
       {product && (
         <div className='p-grid__row'>
-          <div className={classes.sliderBox}></div>
+          <div className={classes.sliderBox}>
+            <ImageSwiper images={product.images} />
+          </div>
           <div className={classes.detail}>
             <h2 className='u-text__headline'>{product.name}</h2>
             <p className={classes.price}>{product.price && product.price.toLocalString}</p>
