727a3ce154e54dff71d75d39e1eac20e33f1d542
zumi0102 <tanakafuka0102@gmail.com>
2023-03-04 17:31:31 +0900

    [chapter21] カート画面に商品表示・戻る機能などの設定

--- src/templates/CartList.jsx
+++ src/templates/CartList.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import List from '@material-ui/core/List';
 import { getProductsInCart } from '../reducks/users/selectors';
+import { CartListItem } from '../components/products';
+import { PrimaryButton, GreyButton } from '../components/UIkit/';
+import { push } from 'connected-react-router';
 
 const CartList = () => {
+  const dispatch = useDispatch();
   const selector = useSelector((state) => state);
   const productsInCart = getProductsInCart(selector);
+
+  const goToOrder = useCallback(() => {
+    dispatch(push('order/confirm'));
+  }, []);
+
+  const backToOrder = useCallback(() => {
+    dispatch(push('/'));
+  }, []);
 
   return (
     <section className='c-section-wrapin'>
       <h2 className='u-text__headline'>ショッピングカート</h2>
-      <List>{productsInCart.legngth > 0 && productsInCart.map((products) => {})}</List>
+      <List>
+        {productsInCart.length > 0 &&
+          productsInCart.map((product) => <CartListItem product={product} key={product.cartId} />)}
+      </List>
+      <div className='module-spacer--medium'>
+        <div className='p-grid__column'>
+          <PrimaryButton labeL={'レジへ進む'} onClick={goToOrder} />
+          <div className='module-spacer--extra-extra-small' />
+          <GreyButton labeL={'ショッピングを続ける'} onClick={backToOrder} />
+        </div>
+      </div>
     </section>
   );
 };
